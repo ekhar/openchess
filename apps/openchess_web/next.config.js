026719f4462b07c9f90887c21ea9177d656cdd6e
Eric Khar <ericsrealemail@gmail.com>
2025-02-27 22:49:48 -0500

    clean up serverside wasm config in next now that its not needed

--- apps/openchess_web/next.config.js
+++ apps/openchess_web/next.config.js
@@ -7,25 +7,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  webpack: (config, { isServer }) => {
-    // Enable .wasm file handling on the server side
-    config.resolve = {
-      ...config.resolve, // Preserve existing resolve configurations
-      symlinks: false,
-    };
-    // config.infrastructureLogging = { debug: /PackFileCache/ };
-    config.cache = false;
-    if (isServer) {
-      config.experiments = {
-        ...config.experiments,
-        asyncWebAssembly: true,
-      };
-    }
-    // Remove client-side .wasm handling if not needed
-    return config;
-  },
-  plugins: [
-  ],
 };
 
 export default nextConfig;
